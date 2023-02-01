77aaac274cda744dc85076cea871497c3c1a55a4
Federico Paglia <federicoepaglia@gmail.com>
2023-02-01 20:33:56 -0300

    desafio3

--- src/app.js
+++ src/app.js
@@ -11,7 +11,14 @@ app.use(express.urlencoded({extended: true}));
 //peticiones
 app.get('/products', async (req,res)=> {
     const products = await productManager.getProducts();
-    res.send({products});
+    const limit = Number(req.query.limit);
+
+    if (!limit){
+        res.send({products})
+    }else {
+        const limitedProducts = products.slice(0, limit);
+        res.send({limitedProducts});
+    }
 })
 
 app.get('/products/:pid', async (req, res) =>{
